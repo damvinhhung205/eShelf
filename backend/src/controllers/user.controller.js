d4e5cf980cd1e45f890f039ade33bcd8952356af
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-03-15 23:53:55 +0700

    feat: fix user controller & update user routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,30 +1,42 @@
 import ApiError from "../api-error.js";
 import UserService from "../services/user.service.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const userService = new UserService();
 dotenv.config();
 
 export async function create(req, res, next) {
-    try {
-        if (!req.body?.username || !req.body?.password || !req.body?.firstName || !req.body?.lastName) {
-            return next(new ApiError(400, "Username, password, first name, and last name are required"));
-        }
-
-        const existingUser = await userService.findByUsername(req.body.username);
-        if (existingUser) {
-            return next(new ApiError(409, "Username already exists"));
-        }
-
-        await userService.create(req.body);
-
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, "An error occurred while registering the user"));
+  try {
+    if (
+      !req.body?.username ||
+      !req.body?.password ||
+      !req.body?.firstName ||
+      !req.body?.lastName
+    ) {
+      return next(
+        new ApiError(
+          400,
+          "Username, password, first name, and last name are required",
+        ),
+      );
     }
+
+    const existingUser = await userService.findByUsername(req.body.username);
+    if (existingUser) {
+      return next(new ApiError(409, "Username already exists"));
+    }
+
+    await userService.create(req.body);
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while registering the user"),
+    );
+  }
 }
 
 export async function findAll(req, res, next) {
@@ -43,108 +55,129 @@ export async function findAll(req, res, next) {
       new ApiError(500, "An error occurred while retrieving the list of users"),
     );
   }
+  return res.json(documents);
 }
 
 export async function findOne(req, res, next) {
-    try {
-        const document = await userService.findById(req.params.id);
+  try {
+    const document = await userService.findById(req.params.id);
 
-        if (!document) {
-            return next(new ApiError(404, "User not found"));
-        }
-
-        return res.json(document);
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, `An error occurred while retrieving the user with id=${req.params.id}`));
+    if (!document) {
+      return next(new ApiError(404, "User not found"));
     }
+
+    return res.json(document);
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(
+        500,
+        `An error occurred while retrieving the user with id=${req.params.id}`,
+      ),
+    );
+  }
 }
 
 export async function update(req, res, next) {
-    try {
-        if (Object.keys(req.body).length === 0) {
-            return next(new ApiError(400, "Data to update cannot be empty"));
-        }
-
-        const document = await userService.update(req.params.id, req.body);
-
-        if (!document) {
-            return next(new ApiError(404, "User not found"));
-        }
-
-        return res.json({ message: "User updated successfully" });
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, `An error occurred while updating the user with id=${req.params.id}`));
+  try {
+    if (Object.keys(req.body).length === 0) {
+      return next(new ApiError(400, "Data to update cannot be empty"));
     }
+
+    const document = await userService.update(req.params.id, req.body);
+
+    if (!document) {
+      return next(new ApiError(404, "User not found"));
+    }
+
+    return res.json({ message: "User updated successfully" });
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(
+        500,
+        `An error occurred while updating the user with id=${req.params.id}`,
+      ),
+    );
+  }
 }
 
 export async function deleteOne(req, res, next) {
-    try {
-        const document = await userService.delete(req.params.id);
+  try {
+    const document = await userService.delete(req.params.id);
 
-        if (!document) {
-            return next(new ApiError(404, "User not found"));
-        }
-
-        return res.json({ message: "User deleted successfully" });
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, `An error occurred while deleting the user with id=${req.params.id}`));
+    if (!document) {
+      return next(new ApiError(404, "User not found"));
     }
+
+    return res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(
+        500,
+        `An error occurred while deleting the user with id=${req.params.id}`,
+      ),
+    );
+  }
 }
 
 export async function deleteAll(req, res, next) {
-    try {
-        const deletedCount = await userService.deleteAll();
+  try {
+    const deletedCount = await userService.deleteAll();
 
-        return res.json({ message: `${deletedCount} users were deleted successfully` });
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, "An error occurred while deleting all users"));
-    }
+    return res.json({
+      message: `${deletedCount} users were deleted successfully`,
+    });
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, "An error occurred while deleting all users"),
+    );
+  }
 }
 
 export async function login(req, res, next) {
-    try {
-        const { username, password } = req.body;
-        if (!username || !password) {
-            return next(new ApiError(400, "Username or password cannot be empty"));
-        }
-
-        const user = await userService.findByUsername(username);
-        if (!user) {
-            return next(new ApiError(401, "Invalid username or password"));
-        }
-
-        const passwordIsCorrect = await bcrypt.compare(password, user.password);
-        if (!passwordIsCorrect) {
-            return next(new ApiError(401, "Invalid username or password"));
-        }
-
-        // Generate JWT_SECRET later
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        return res.status(200).json({
-            token,
-            user: {
-                _id: user._id,
-                username: user.username,
-                role: user.role
-            }
-        })
-    } catch (error) {
-        console.log(error);
-        return next(new ApiError(500, "An error occurred while logging in"));
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return next(new ApiError(400, "Username or password cannot be empty"));
     }
+
+    const user = await userService.findByUsername(username);
+    if (!user) {
+      return next(new ApiError(401, "Invalid username or password"));
+    }
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+    if (!passwordIsCorrect) {
+      return next(new ApiError(401, "Invalid username or password"));
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return next(new ApiError(500, "An error occurred while logging in"));
+  }
 }
 
 export default {
-    create,
-    findAll,
-    findOne,
-    update,
-    deleteOne,
-    deleteAll,
-    login
+  create,
+  findAll,
+  findOne,
+  update,
+  deleteOne,
+  deleteAll,
+  login,
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import auth from '../middlewares/auth.middleware.js';
 import UserController from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post("/register", UserController.create);
+router.post("/login", UserController.login);
+
+router.route("/")
+    .post(UserController.create)
+    .get(auth, UserController.findAll)
+    .delete(auth, UserController.deleteAll)
+
+router.route("/:id")
+    .get(auth, UserController.findOne)
+    .put(auth, UserController.update)
+    .delete(auth, UserController.deleteOne)
 
 export default router;
