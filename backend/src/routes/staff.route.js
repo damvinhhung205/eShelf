1ceb95c4901cd77de1c7aeff2d461bd0c35bc9af
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-03-17 00:02:05 +0700

    feat: add staff.route

---
+++ backend/src/routes/staff.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import auth from "../middlewares/auth.middleware.js";
+import staffController from "../controllers/staff.controller";
+
+const router = express.Router();
+
+router.post("/register", staffController.create);
+router.post("/login", staffController.login);
+
+router.route("/")
+    .post(staffController.create)
+    .get(auth, staffController.findAll)
+    .delete(auth, staffController.deleteAll);
+
+router.route(":/id")
+    .get(auth, staffController.findOne)
+    .put(auth, staffController, update)
+    .delete(auth, staffController.deleteOne);
+
+export default router;
