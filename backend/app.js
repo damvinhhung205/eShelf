bc234c709c344dcb751d57b12007d92caeaa906d
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-03-17 21:43:21 +0700

    feat: add publisher route in app

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,31 @@
-import APiError from './src/api-error.js';
-import express from 'express';
-import cors from 'cors';
+import APiError from "./src/api-error.js";
+import express from "express";
+import cors from "cors";
 
-import userRouter from './src/routes/user.route.js';
-import staffRouter from './src/routes/staff.route.js';
+import userRouter from "./src/routes/user.route.js";
+import staffRouter from "./src/routes/staff.route.js";
+import publisherRouter from "./src/routes/publisher.route.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to eShelf"});
+  res.json({ message: "Welcome to eShelf" });
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/staff", staffRouter);
+app.use("/api/publisher", publisherRouter);
 
 app.use((req, res, next) => {
-    return next(new APiError(404, "Resource not found"));
+  return next(new APiError(404, "Resource not found"));
 });
 
 app.use((error, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error"
-    });
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 
 export default app;
