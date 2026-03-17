c5722e5aa1df55183d81d74241ec79a865a6c8da
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-03-17 21:39:58 +0700

    feat: add publisher.route

---
+++ backend/src/routes/publisher.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import auth from "../middleware/auth.middleware.js";
+import PublisherController from "../controllers/publisher.controller.js";
+
+const router = express.Router();
+
+router
+  .route("/")
+  .post(auth, PublisherController.create)
+  .get(PublisherController.findAll)
+  .delete(auth, PublisherController.deleteAll);
+
+router
+  .route("/:id")
+  .get(PublisherController.findOne)
+  .put(auth, PublisherController.update)
+  .delete(auth, PublisherController.deleteOne);
+
+export default router;
