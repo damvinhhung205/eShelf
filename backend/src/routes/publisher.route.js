b0555087eea97ccd6c9b1f3f612a973342ecfbd8
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-03-22 18:05:46 +0700

    feat: add borrow.route & update publisher.route

--- backend/src/routes/publisher.route.js
+++ backend/src/routes/publisher.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import auth from "../middleware/auth.middleware.js";
+import auth from "../middlewares/auth.middleware.js";
 import PublisherController from "../controllers/publisher.controller.js";
 
 const router = express.Router();
