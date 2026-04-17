8dd5b8a9fc2a43d2aeee0ec7232b89dfac5bd4cb
damvinhhung205 <damvinhhung9a2@gmail.com>
2026-04-18 01:29:49 +0700

    feat: add home view & not found view

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col min-h-screen transition-all">
+    <Header></Header>
+    <div class="hero grow bg-[url(/images/t2_sach_2.jpg)] transition-all duration-300">
+      <div class=" hero-overlay backdrop-brightness-60 backdrop-blur-xs"></div>
+      <div class="hero-content text-neutral-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold"> eShelf <br /> Kệ sách điện tử</h1>
+          <p class="text-2xl my-8">
+            Kết nối dễ dàng với kho tàng kiến thức.
+          </p>
+          <RouterLink to="/books" class="shadow font-bold text-base btn btn-lg hover:underline hover:shadow-md">Truy
+            cập
+          </RouterLink>
+
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+</script>
